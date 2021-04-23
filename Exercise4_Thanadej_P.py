68b92f65a55fe6e6734d7c1f2b41dff52b3c25e4
Purk-coder <77494699+Purk-coder@users.noreply.github.com>
2021-04-23 16:50:45 +0700

    Add files via upload

---
+++ Exercise4_Thanadej_P.py
@@ -0,0 +1,10 @@
+F = 85.5
+G = 70.0
+I = 50.5
+C = 50.0
+
+print("--- Your Score---")
+print("Foundation English " , F )
+print("General Business", G)
+print("Introduction to Computer Systems", I)
+print("Computer Programming", C)
